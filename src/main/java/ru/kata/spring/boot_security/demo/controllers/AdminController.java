2ac745725ec5a7bcdda0621088b937a4b70d750e
Ahmed-1109 <m.magomedov1999@mail.ru>
2024-02-25 15:43:09 +0300

    valid1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -0,0 +1,70 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.services.RoleService;
+import ru.kata.spring.boot_security.demo.services.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private UserService userService;
+    private final RoleService roleService;
+
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping()
+    public String getAllUsers(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "allUsers";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(@PathVariable(name = "id") int id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        model.addAttribute("roles",roleService.getAllRoles());
+    return "editUser";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String update(@ModelAttribute("user") User user,
+                         @PathVariable("id") int id,
+                         @ModelAttribute("roles") List<Integer> rolesId) {
+        if (!rolesId.isEmpty()) {
+            List<Role> roles = new ArrayList<>();
+            for (int roleId : rolesId) {
+                roles.add(roleService.getRoleById(roleId));
+            }
+            user.setRoles(roles);
+        }
+        userService.update(id, user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/addUser")
+    public String newPerson(@ModelAttribute("user") User user, Model model) {
+        model.addAttribute("roles", roleService.getAllRoles());
+        return "addUser";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("user") User user, @ModelAttribute("roles") List<Integer> rolesId) {
+        List<Role> roles = new ArrayList<>();
+        for (int roleId : rolesId) {
+            roles.add(roleService.getRoleById(roleId));
+        }
+        user.setRoles(roles);
+        userService.save(user);
+        return "redirect:/admin";
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -0,0 +1,70 @@
+package ru.kata.spring.boot_security.demo.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.configs.PasswordConfig;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private UserRepository userRepository;
+    private final PasswordConfig passwordConfig;
+
+
+    public UserServiceImpl(UserRepository userRepository,  PasswordConfig passwordConfig) {
+        this.userRepository = userRepository;
+        this.passwordConfig = passwordConfig;
+
+    }
+
+    @Override
+    public User getUserById(int id) {
+        return userRepository.getUserById(id);
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+       user.setPassword(passwordConfig.passwordEncoder().encode(user.getPassword()));
+       userRepository.save(user);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.getUserByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return getUserByUsername(username);
+    }
+
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    @Override
+    @Transactional
+    public void update(int id, User user) {
+        User user1 = getUserById(user.getId());
+        if (!user1.getPassword().equals(user.getPassword())){
+        user.setPassword(passwordConfig.passwordEncoder().encode(user.getPassword()));
+        }
+        userRepository.update(id,user);
+    }
+
+    @Override
+    @Transactional
+    public void removeUser(int id) {
+        userRepository.remove(id);
+    }
+}
